8d039eaae32a228df26c5775cbbb8b17eb52bd99
Badr-Eddine Marani <badr-eddine.marani@outlook.com>
2025-01-05 16:30:46 +0000

    added wandb for metric tracking

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import argparse
+import gc
 import os
-from typing import List, Literal, Tuple
+from typing import Any, Dict, List, Literal, Optional, Tuple
 
 import torch
+import wandb
 from datasets import load_dataset
 from huggingface_hub import PyTorchModelHubMixin
 from safetensors.torch import safe_open, save_file
 from torch import nn
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset, random_split
+from tqdm import trange
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-def get_device(device_id: int = None) -> Tuple[bool, torch.device]:
+def get_device(device_id: Optional[int] = None) -> Tuple[bool, torch.device]:
     if torch.cuda.is_available():
-        use_cuda, device = (
-            True,
-            f"cuda:{device_id}" if device_id is not None else "cuda",
-        )
+        use_cuda = True
+        if device_id is not None and isinstance(device_id, int):
+            device = f"cuda:{device_id}"
+        else:
+            device = "cuda"
     else:
         use_cuda, device = False, "cpu"
     return use_cuda, torch.device(device)
@@ -44,7 +48,7 @@ def cache_activations(
     use_cuda, device = get_device(device_id)
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     model = AutoModelForCausalLM.from_pretrained(
-        model_name_or_path, device_map={}
+        model_name_or_path, device_map={"": device}
     )
 
     activations, shard_index = [], 1
@@ -53,6 +57,7 @@ def cache_activations(
     for i in range(0, len(dataset), num_steps):
         x = dataset[i : i + num_steps]["train"]
         inputs = tokenizer(x, return_tensors="pt")
+        inputs = {k: v.to(device=device) for k, v in inputs.items()}
         hidden_states = (
             model(**inputs, output_hidden_states=True)
             .hidden_states[model_layer_index]
@@ -65,6 +70,9 @@ def cache_activations(
 
     if len(activations) > 0:
         save_shard(activations, shard_path, shard_index)
+    del model
+    torch.cuda.empty_cache()
+    gc.collect()
 
 
 def load_cached_activations(shard_path: str) -> torch.Tensor:
@@ -85,64 +93,104 @@ class SAE(nn.Module, PyTorchModelHubMixin):
         self,
         acts_size: int,
         dict_size: int,
-        topk: Literal["soft", "hard"] = "soft",
+        mode: Literal["relu", "topk"] = "relu",
         k: int = 10,
+        l1_coef: float = 1.0,
     ) -> None:
         super().__init__()
-        self.topk = topk
+        self.mode = mode
         self.k = k
+        self.l1_coef = l1_coef
         self.encode = nn.Linear(acts_size, dict_size)
         self.decode = nn.Linear(dict_size, acts_size, bias=False)
 
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        pre_acts = self.encode(input)
-        if self.topk == "soft":
-            acts = pre_acts.relu()
-        elif self.topk == "hard" and self.k is not None:
-            _, indices = torch.topk(pre_acts, self.k, dim=1)
-            acts = torch.zeros_like(pre_acts)
-            acts.scatter_(1, indices, pre_acts)
+    def forward(self, input: torch.Tensor) -> Dict[str, torch.Tensor]:
+        acts = self.encode(input)
+        if self.mode == "relu":
+            acts = acts.relu()
+        elif self.mode == "topk" and self.k is not None:
+            _, indices = torch.topk(acts, self.k, dim=-1)
+            acts *= torch.zeros_like(acts).scatter_(-1, indices, 1)
         recon_input = self.decode(acts)
-        return recon_input, acts
+        loss_dict = self.loss_fn(input, recon_input, acts)
+        return {
+            "recon_input": recon_input,
+            "input": input,
+            "activation": acts,
+            **loss_dict,
+        }
 
-
-def sae_loss_fn(
-    input: torch.Tensor,
-    recon_input: torch.Tensor,
-    acts: torch.Tensor,
-    l1_coef: float = 1.0,
-    topk: Literal["soft", "hard"] = "soft",
-) -> torch.Tensor:
-    loss = nn.functional.mse_loss(input, recon_input)
-    if topk == "soft":
-        loss += l1_coef * torch.mean(acts)
-    return loss
+    def loss_fn(
+        self,
+        input: torch.Tensor,
+        recon_input: torch.Tensor,
+        acts: torch.Tensor,
+    ) -> Dict[str, torch.Tensor]:
+        loss = nn.functional.mse_loss(input, recon_input)
+        out = {
+            "loss": loss,
+            "l1_loss": torch.mean(acts),
+            "l0_loss": (acts != 0).float().mean(),
+        }
+        if self.mode == "relu":
+            loss += self.l1_coef * out["l1_loss"]
+        return out
 
 
 def main(
-    sae_model_name: str,
+    wandb_project: str,
+    run_name: str,
     model_name_or_path: str,
     dataset_name_or_path: str,
     activations_path: str,
-    shard_size: int,
     acts_size: int,
-    dict_size: int,
-    batch_size: int,
-    learning_rate: float,
-    num_epochs: int,
-    model_layer_index: int,
+    dict_size: Optional[int] = None,
+    expansion_factor: int = 4,
+    shard_size: int = 10_000,
+    batch_size: int = 2048,
+    learning_rate: float = 1e-4,
+    num_epochs: int = 10,
+    model_layer_index: int = 8,
     device_id: int = None,
-    l1_coef: float = 1.0,
-    topk: Literal["soft", "hard"] = "soft",
+    l1_coef: float = 1e-2,
+    mode: Literal["relu", "topk"] = "topk",
     k: int = 10,
+    log_interval: int = 100,
 ):
+    metadata = {
+        "wandb_project": wandb_project,
+        "run_name": run_name,
+        "model_name_or_path": model_name_or_path,
+        "dataset_name_or_path": dataset_name_or_path,
+        "activations_path": activations_path,
+        "shard_size": shard_size,
+        "acts_size": acts_size,
+        "dict_size": dict_size,
+        "batch_size": batch_size,
+        "expansion_factor": expansion_factor,
+        "learning_rate": learning_rate,
+        "num_epochs": num_epochs,
+        "model_layer_index": model_layer_index,
+        "device_id": device_id,
+        "l1_coef": l1_coef,
+        "mode": mode,
+        "k": k,
+        "log_interval": log_interval,
+    }
+    wandb.init(project=wandb_project, name=run_name, config=metadata)
+
     use_cuda, device = get_device(device_id)
-    model = SAE(acts_size=acts_size, dict_size=dict_size, topk=topk, k=k)
-    model.to(device)
+    if dict_size is None:
+        dict_size = acts_size * expansion_factor
+    model = SAE(acts_size, dict_size, mode, k, l1_coef)
+    model.to(device=device)
+
+    # to automatically log gradients
+    wandb.watch(model, log_freq=100)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    if activations is None or not os.path.exists(activations_path):
+    if activations_path is None or not os.path.exists(activations_path):
         cache_activations(
             model_name_or_path=model_name_or_path,
             dataset_name_or_path=dataset_name_or_path,
@@ -154,42 +202,74 @@ def main(
         )
     activations = load_cached_activations(activations_path)
     dataset = TensorDataset(activations)
-    loader = DataLoader(dataset, batch_size=batch_size, pin_memory=True)
+    train_dataset, eval_dataset = random_split(dataset, [0.8, 0.2])
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+    eval_loader = DataLoader(eval_dataset, batch_size, shuffle=False)
 
-    for i in range(num_epochs):
-        for (x,) in loader:
+    for i in trange(num_epochs, desc="Epochs"):
+        model.train()
+        for batch_index, (x,) in enumerate(train_loader, start=1):
             x = x.to(device=device)
-            recon_x, acts = model(x)
-            loss = sae_loss_fn(x, recon_x, acts, l1_coef=l1_coef, topk=topk)
-            loss.backward()
+            output = model(x)
+            output["loss"].backward()
             optimizer.step()
             optimizer.zero_grad()
 
-        model.save_pretrained(sae_model_name)
+            if batch_index % log_interval == 0:
+                wandb.log(
+                    {
+                        "loss/train": output["loss"].item(),
+                        "l1_loss/train": output["l1_loss"].item(),
+                        "l0_loss/train": output["l0_loss"].item(),
+                    }
+                )
+
+        model.eval()
+        with torch.no_grad():
+            t = {"loss": 0.0, "l1_loss": 0.0, "l0_loss": 0.0}
+            for batch_index, (x,) in enumerate(eval_loader, start=1):
+                x = x.to(device=device)
+                output = model(x)
+                t["loss"] += output["loss"].item()
+                t["l1_loss"] += output["l1_loss"].item()
+                t["l0_loss"] += output["l0_loss"].item()
+
+            wandb.log(
+                {
+                    "loss/eval": t["loss"] / len(eval_loader),
+                    "l1_loss/eval": t["l1_loss"] / len(eval_loader),
+                    "l0_loss/eval": t["l0_loss"] / len(eval_loader),
+                }
+            )
+    model.save_pretrained(run_name)
+    wandb.finish()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train SAE model")
-    parser.add_argument("--saex_model_name", type=str)
+    parser.add_argument("--run_name", type=str)
     parser.add_argument("--model_name_or_path", type=str)
     parser.add_argument("--dataset_name_or_path", type=str)
     parser.add_argument("--activations_path", type=str)
-    parser.add_argument("--shard_size", type=int)
+    parser.add_argument("--shard_size", type=int, default=10_000)
     parser.add_argument("--acts_size", type=int)
-    parser.add_argument("--dict_size", type=int)
-    parser.add_argument("--batch_size", type=int)
-    parser.add_argument("--learning_rate", type=float)
-    parser.add_argument("--num_epochs", type=int)
-    parser.add_argument("--model_layer_index", type=int)
-    parser.add_argument("--device_id", type=int, default=None)
-    parser.add_argument("--l1_coef", type=float, default=1.0)
-    parser.add_argument("--topk", type=str, default="soft")
+    parser.add_argument("--dict_size", type=int, required=False)
+    parser.add_argument("--expansion_factor", type=int, default=4)
+    parser.add_argument("--batch_size", type=int, default=2048)
+    parser.add_argument("--learning_rate", type=float, default=1e-4)
+    parser.add_argument("--num_epochs", type=int, default=10)
+    parser.add_argument("--model_layer_index", type=int, default=8)
+    parser.add_argument("--device_id", type=int, default=0)
+    parser.add_argument("--l1_coef", type=float, default=1e-2)
+    parser.add_argument("--mode", type=str, default="topk")
     parser.add_argument("--k", type=int, default=10)
+    parser.add_argument("--log_interval", type=int, default=100)
 
     args = parser.parse_args()
 
     main(
-        sae_model_name=args.sae_model_name,
+        wandb_project="sae",
+        run_name=args.run_name,
         model_name_or_path=args.model_name_or_path,
         dataset_name_or_path=args.dataset_name_or_path,
         activations_path=args.activations_path,
@@ -202,6 +282,7 @@ if __name__ == "__main__":
         model_layer_index=args.model_layer_index,
         device_id=args.device_id,
         l1_coef=args.l1_coef,
-        topk=args.topk,
+        mode=args.mode,
         k=args.k,
+        log_interval=args.log_interval,
     )
--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 python main.py \
-    --model_name_or_path "path/to/pretrained/model" \
-    --dataset_name_or_path "path/to/dataset" \
-    --activations_path "path/to/activations" \
-    --shard_size 1000 \
-    --acts_size 500 \
-    --dict_size 1000 \
-    --batch_size 32 \
-    --learning_rate 0.001 \
+    --run_name topksae_gpt2_layer8_tiny_shakespeare \
+    --model_name_or_path openai-community/gpt2 \
+    --dataset_name_or_path tiny_shakespeare \
+    --shard_size 10_000 \
+    --acts_size 768 \
+    --expansion_factor 4 \
+    --batch_size 2048 \
+    --learning_rate 1e-4 \
     --num_epochs 10 \
-    --model_layer_index 0 \
+    --model_layer_index 8 \
     --device_id 0 \
-    --l1_coef 1.0 \
-    --topk "soft" \
-    --k 10
+    --l1_coef 1e-2 \
+    --mode topk \
+    --log_interval 100
